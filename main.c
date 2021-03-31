96ea29d47156f8178e64b86ec32dc055a186b73f
Cyan Sun <62244146+NavigatorKepler@users.noreply.github.com>
2021-04-01 00:45:58 +0800

    基本完成要求

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 
 struct POSITION {
-    uint64_t code;
+    uint32_t code;
     uint8_t local[64];
 } __preread, __current, position_list[LISTMAX];
 
@@ -34,39 +35,48 @@ int LoadList() {
     return 0;
 }
 
-int64_t searchIndex(uint64_t code) {
-    size_t imax=listptr;
-    size_t imin=0;
-    size_t index=(imax + imin) / 2;
+int32_t searchIndex(uint32_t code) {
+    int32_t imax=listptr;
+    int32_t imin=0;
+    int32_t index=(imax + imin) / 2;
     __current=position_list[index];
     while (imin < imax) {
+        printf("code=%d, imin=%d, imax=%d, ptr=%d\r", code, imin, imax, index);
         if (__current.code == code) {
+            // printf("\n");
             return index;
-        } else if (__current.code > code){
+        } else if (__current.code > code) {
             imax -= (imax - imin) / 2;
-        } else if (__current.code < code){
+        } else if (__current.code < code) {
             imin += (imax - imin) / 2;
         }
         index=(imax + imin) / 2;
         __current=position_list[index];
     }
-    return -1;
+    if (__current.code == code) {
+        // printf("\n");
+        return index;
+    } else {
+        return -1;
+    }
 }
 
 void main() {
     LoadList();
 
-    char c;
-    char stopflag=0;
-    char IDtemp[19];
-    uint64_t location, biry, birm, bird, cnt, chk;
+    uint8_t  c;
+    uint8_t  stopflag=0;
+    uint8_t  IDtemp[19];
+    uint32_t _T1, _T2;
+    uint32_t location=0, biry=0, birm=0, bird=0, cnt=0, chk=0;
+    int32_t  realLocationID1, realLocationID2, realLocationID3;
 
-    uint8_t weight[] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
-    uint8_t ZtoM[] = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
+    uint8_t  weight[17] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+    uint8_t  ZtoM[11] = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
 
     // 用于验证二分查找
     // for (int i=0;i<listptr;i++) {
-    //     int64_t searchCode = searchIndex(position_list[i].code);
+    //     int32_t searchCode = searchIndex(position_list[i].code);
     //     if (searchCode != i) {
     //         printf("在%d位置发生错误, 实际区号为%d，查找到的序号是%d，区号是%d\n", i, position_list[i].code, searchCode, position_list[searchCode].code);
     //     };
@@ -84,7 +94,7 @@ void main() {
         if (stopflag == 1) continue;
 
 
-        for (char i=0;i<17;i++) {
+        for (uint32_t i=0;i<17;i++) {
             if (IDtemp[i]-'0'<0 || IDtemp[i]-'9'>9) {
                 printf("输入中混入了非法字符!\n");
                 stopflag=1;
@@ -104,11 +114,51 @@ void main() {
 
         char res=chk%11;
         if (ZtoM[res] != IDtemp[17]) {
-            // printf("%s\n",ZtoM[res]);
             printf("输入的身份证%s不合法!\n",IDtemp);
             stopflag=1;
         }
         if (stopflag == 1) continue;
 
+        for (uint32_t i=0;i<6;i++) {
+            location*=10;
+            location+=IDtemp[i]-'0';
+        }
+        for (uint32_t i=0;i<4;i++) {
+            biry*=10;
+            biry+=IDtemp[i+6]-'0';
+        }
+        for (uint32_t i=0;i<2;i++) {
+            birm*=10;
+            birm+=IDtemp[i+10]-'0';
+        }
+        for (uint32_t i=0;i<2;i++) {
+            bird*=10;
+            bird+=IDtemp[i+12]-'0';
+        }
+        for (uint32_t i=0;i<3;i++) {
+            cnt*=10;
+            cnt+=IDtemp[i+14]-'0';
+        }
+
+        _T1 = location/10000;
+        _T2 = location/100;
+        _T1 *= 10000;
+        _T2 *= 100;
+        realLocationID1 = searchIndex(_T1);
+        realLocationID2 = searchIndex(_T2);
+        realLocationID3 = searchIndex(location);
+        if (realLocationID3 == -1 || realLocationID2 == -1 || realLocationID1 == -1) {
+            printf("身份证的地区无效!\n");
+            stopflag=1;
+        } else {
+            printf("地区代码:%d; %s, %s, %s\n", location, position_list[realLocationID1].local, position_list[realLocationID2].local, position_list[realLocationID3].local);
+        }
+        if (stopflag == 1) continue;
+        printf("出生于:%d年%d月%d日\n", biry, birm, bird);
+        if (cnt%2) {
+            printf("性别:男\n");
+        } else {
+            printf("性别:女\n");
+        }
     }
 }
